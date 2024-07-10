83f62571ecfd381aef7e2a7fe80b9667145c4a52
ThatYolandaWang <wangying123820@gmail.com>
2024-07-10 16:42:35 +0200

    main

--- myspider/items.py
+++ myspider/items.py
@@ -13,21 +13,3 @@ class OtodomItem(scrapy.Item):
     unit = scrapy.Field()
     floor = scrapy.Field()
     address = scrapy.Field()
-
-class TaobaoItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    class1 = scrapy.Field()
-    class2 = scrapy.Field()
-
-# 这个item是获取具体内容的字段    
-class TaobaoDataItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    title = scrapy.Field()
-    price = scrapy.Field()
-    sales = scrapy.Field()
-    store = scrapy.Field()
-    class1 = scrapy.Field()
-    class2 = scrapy.Field()
-    
